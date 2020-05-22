6659e3c3ed2530b562a7d3a1382fdd07e2d1f188
Raffi Hotter <rhotter@users.noreply.github.com>
2020-05-22 17:35:07 -0400

    pp

--- train_helpers.py
+++ train_helpers.py
@@ -19,9 +19,9 @@ def get_loss_weights(epochs_train):
 
 def load_losses(saved_models_dir, name):
 	with open(op.join(saved_models_dir, name + '_train_losses.npy'), 'rb') as f:
-		train_losses = np.load(f)
+		train_losses = list(np.load(f))
 	with open(op.join(saved_models_dir, name + '_test_losses.npy'), 'rb') as f:
-		test_losses = np.load(f)
+		test_losses = list(np.load(f))
 	return train_losses, test_losses
 
 def save_losses(train_losses, test_losses, saved_models_dir, name):
